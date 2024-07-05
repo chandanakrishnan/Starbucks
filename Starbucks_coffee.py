701412fd3489cbf59bd471dd60fcaca356b6e2ad
chandana <chandanabhattar@gmail.com>
2024-07-05 16:46:24 -0400

    adding the file Starbucks_coffee.py to Starbucks folder

---
+++ Starbucks_coffee.py
@@ -0,0 +1,45 @@
+coffee = str(input("Choose the coffee you want :iced caramel brulee latte, matcha latte, cappucino? "))
+temperature = str(input("Do you want it hot or cold? "))
+size = str(input("please select the size : S,M,L? "))
+
+#iced caramel brulee latte
+if(coffee=='iced caramel brulee latte' and temperature=='hot'and size=='S'):
+  print("Final price = $5.5")
+elif (coffee=='iced caramel brulee latte'and temperature=='hot' and size=='M'):
+  print("Final price = $6.5")
+elif(coffee=='iced caramel brulee latte'and temperature=='hot' and size=='L'):
+  print("Final price = $7.5")
+elif(coffee=='iced caramel brulee latte' and temperature=='cold'and size=='S'):
+  print("Final price = $5")
+elif (coffee=='iced caramel brulee latte'and temperature=='cold' and size=='M'):
+  print("Final price = $6")
+elif(coffee=='iced caramel brulee latte'and temperature=='cold' and size=='L'):
+  print("Final price = $7")
+
+#matcha latte
+if(coffee=='matcha latte' and temperature=='hot'and size=='S'):
+  print("Final price = $5.5")
+elif (coffee=='matcha latte'and temperature=='hot' and size=='M'):
+  print("Final price = $6.5")
+elif(coffee=='matcha latte'and temperature=='hot' and size=='L'):
+  print("Final price = $7.5")
+elif(coffee=='matcha latte' and temperature=='cold'and size=='S'):
+  print("Final price = $5")
+elif (coffee=='matcha latte'and temperature=='cold' and size=='M'):
+  print("Final price = $6")
+elif(coffee=='matcha latte'and temperature=='cold' and size=='L'):
+  print("Final price = $7")
+
+#cappucino
+if(coffee=='cappucino' and temperature=='hot'and size=='S'):
+  print("Final price = $5.5")
+elif (coffee=='cappucino'and temperature=='hot' and size=='M'):
+  print("Final price = $6.5")
+elif(coffee=='cappucino'and temperature=='hot' and size=='L'):
+  print("Final price = $7.5")
+elif(coffee=='cappucino' and temperature=='cold'and size=='S'):
+  print("Final price = $5")
+elif (coffee=='cappucino'and temperature=='cold' and size=='M'):
+  print("Final price = $6")
+elif(coffee=='cappucino'and temperature=='cold' and size=='L'):
+  print("Final price = $7")
